6c1703d1e9610bac82d4912dcafffb6d6e61b265
Casey Dailey <casey@feasttogether.org>
2017-09-02 12:35:01 -0500

    clarified comments in app.js

--- app/app.js
+++ app/app.js
@@ -3,10 +3,17 @@
 // instatiate the module as 'app'
 const app = angular.module("TodoApp", ['ngRoute']);
 
-// ckecks to see if a user is logged in
+// is user logged in?
 // this is checked as the 'resolve' in most views
+// // the resolve is an optional map of dependencies if they are resolved successfully,
+// they will be injected when the controller is instantiated, and are available to $scope in that controller under $resolve.
+// else a $routeChangeError will be fired
+// in this case, we need to know if the user is logged in to determine whether to allow access to certain paths
 const isAuth = (userFactory) => userFactory.isAuthenticated();
 
+// using $routeProvider to configure the routes
+// the .when specifies the the template, controller, and the resolve (see above)
+// to be instantiated when the path is requested
 app.config(($routeProvider)=> {
     $routeProvider 
     .when('/', {
@@ -45,5 +52,8 @@ app.config(($routeProvider)=> {
     .otherwise('/');
 });
 
-// .run
+// .run blocks - A run block is the code which needs to run to kickstart the application. 
+// It is executed after all of the service have been configured and the injector has been created
+// here we are just initializing our app with firebase, passing 'FRCreds', a constant registered in app/fb-creds.js
+// which contains the databaseURL, apiKey, and authDomain need to interact with the app
 app.run(($location, FBCreds)=> firebase.initializeApp(FBCreds));
